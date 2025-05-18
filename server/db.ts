d97604c258ff4c03a62446c31af876297b602735
realityinspector <mcdonald.sean@gmail.com>
2025-05-18 15:45:20 -0700

    update neon config 1a

--- server/db.ts
+++ server/db.ts
@@ -7,7 +7,7 @@ const getDatabaseUrl = () => {
   let dbUrl = process.env.DATABASE_URL;
   
   if (!dbUrl) {
-    console.error('DATABASE_URL environment variable is not set');
+    console.error('DATABASE_URL environment  variable is not set');
     throw new Error('DATABASE_URL environment variable is not set');
   }
   
